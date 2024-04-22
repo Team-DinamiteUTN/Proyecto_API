5d9b3064415ebd1c50d82ada2639fac74a6daa72
William Cubero Navarro <w.cubero800@gmail.com>
2024-04-22 17:42:33 -0600

    SUBIDAPistasBackend

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Load libraries into the environment application
 var createError = require('http-errors');
 var express = require('express');
@@ -15,17 +16,20 @@ mongoose.connect('mongodb://localhost:27017/',{dbName:'dbCubesGame'});
 
 require('./models/cubeModel');
 require('./models/playerModel');
+require('./models/pistaModel.js');
 
 // Creates each route link
 var indexRouter = require('./routes/index.js');
 var cubesRouter = require('./routes/cubeRoute.js');
 var playerRouter = require('./routes/playerRoute.js');
+var pistasRouter = require('./routes/pistasjudadores.js');
+
 
 // Create all listener for each route link
 app.use('/', indexRouter);
 app.use('/cube', cubesRouter);
 app.use('/player', playerRouter);
-
+app.use('/pista', pistasRouter);
 // Execute local API server and create listener on port 5005
 var server = app.listen(3000, () => {
     console.log(`Server is listening on port ${server.address().port}`);
